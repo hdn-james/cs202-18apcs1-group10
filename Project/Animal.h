2c4a0503348a6b7292cc630c95b20dd540a88783
Đại Nhân HUỲNH <49125797+huynhdainhan@users.noreply.github.com>
2019-11-27 00:31:32 +0700

    Draw some Objects
    * Console + Menu
    
    * Draw Objects

--- Project/Animal.h
+++ Project/Animal.h
@@ -2,6 +2,9 @@
 #define ANIMAL_H
 
 #include <iostream>
+#include <vector>
+
+#include "Console.h"
 using namespace std;
 
 class CANIMAL
@@ -9,20 +12,42 @@ class CANIMAL
 	int mX, mY;
 
 public:
-	virtual void Move(int, int);
+	virtual void moveLeft(int, int);
+	virtual void moveRight(int, int);
+	virtual void deleteObject(int, int);
+
 	virtual void Tell();
 };
 
 class CDINAUSOR : public CANIMAL
 {
+private:
+	char a = 219, b = 220, c = 223; // █ ▄ ▀
+	std::string dinausorToLeft;
+	std::string dinausorToRight;
+
 public:
-	//...
+	CDINAUSOR();
+	void moveLeft(int, int);
+	void moveRight(int, int);
+	void deleteObject(int, int);
+
+	virtual void Tell();
 };
 
 class CBIRD : public CANIMAL
 {
+private:
+	char a = 223, b = 254; // ▀■▀
+	std::string bird;
+
 public:
-	//...
+	CBIRD();
+	void moveLeft(int, int);
+	void moveRight(int, int);
+	void deleteObject(int, int);
+
+	virtual void Tell();
 };
 
 #endif /* Animal.h */
--- Project/menu.cpp
+++ Project/menu.cpp
@@ -1,2 +1,94 @@
 #include "menu.h"
+
+#include "GraphicsHandler.h"
 using namespace std;
+
+menuFunction::menuFunction()
+{
+    numOfItems = 4;
+    MENU.push_back("NEW GAME");
+    MENU.push_back("LOAD GAME");
+    MENU.push_back("SETTING");
+    MENU.push_back("EXIT");
+    hideCursor();
+    drawMainMenu();
+}
+
+menuFunction::~menuFunction()
+{
+    hideCursor();
+    deleteMenu();
+}
+
+void menuFunction::drawMainMenu()
+{
+    cout << this->title;
+	drawBorder();
+	TextColor(Color_Cyan);
+	for (int i = 0; i < numOfItems; i++) {
+		GotoXY(70, i * 2 + 15);
+		cout << MENU[i];
+	}
+}
+
+void menuFunction::deleteMenu()
+{
+    for (int i = 1; i < Y_MAX - 1; i++)
+    {
+        for (int j = 0; j < Y_MAX; j++)
+        {
+            GotoXY(i, j);
+            cout << " ";
+        }
+    }
+}
+
+int getMove()
+{
+    char c = _getwch();
+    if ((int)c == -32)
+        c = _getwch();
+    switch ((int)c)
+    {
+    case 72:
+        return 1; // up
+    case 80:
+        return 2; // down
+    case 75:
+        return 3; // left
+    case 77:
+        return 4; // right
+    case 27:
+        return 5; // esc
+    case 13:
+        return 6; // enter
+    default:
+        return 0;
+    }
+}
+
+void menuFunction::drawBorder()
+{
+    char a = 201, b = 205, c = 187; // ╔ ═ ╗
+    GotoXY(0, 0);
+    cout << a;
+    for (int i = 1; i < (X_MAX); i++)
+    {
+        cout << b;
+    }
+    cout << c;
+    char d = 186, e = 200, f = 188; // ║ ╚ ╝
+    for (int i = 1; i < Y_MAX; i++)
+    {
+        GotoXY(0, i);
+        cout << d;
+        GotoXY(X_MAX, i);
+        cout << d;
+    }
+    cout << e;
+    for (int i = 1; i < (X_MAX); i++)
+    {
+        cout << b;
+    }
+    cout << f;
+}
