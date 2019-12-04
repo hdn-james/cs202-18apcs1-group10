ee962d6f31deb80069207b2a8a535089dfa31550
phamquochuy1102 <50992987+phamquochuy1102@users.noreply.github.com>
2019-12-04 23:00:07 +0700

    Update People.h

--- People.h
+++ People.h
@@ -2,6 +2,9 @@
 #define PEOPLE_H
 
 #include <iostream>
+#include<fstream>
+#include<vector>
+#include<string>
 #include "Animal.h"
 #include "Vehicle.h"
 using namespace std;
@@ -10,16 +13,21 @@ class CPEOPLE
 {
 	int mX, mY;
 	bool mState; // trang thai song chet
+	vector<string> Object;
+	int numPlay;
 public:
 	CPEOPLE();
-	void Up(int);
-	void Left(int);
-	void Right(int);
-	void Down(int);
+	void draw();
+	void removeDraw();
+	void move(char input,int val);
+	void Up(int val);
+	void Left(int val);
+	void Right(int val);
+	void Down(int val);
 	bool isImpact(const CVEHICLE *&);
 	bool isImpact(const CANIMAL *&);
 	bool isFinish();
 	bool isDead();
 };
 
-#endif /* People.h */
+#endif /* People.h *
