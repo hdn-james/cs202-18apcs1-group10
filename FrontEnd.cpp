892e4b7281f269e00ce5358f4b7fb6fe12af4218
HoangNhi0106 <47691938+HoangNhi0106@users.noreply.github.com>
2019-12-08 23:39:53 +0700

    Front End

--- FrontEnd.cpp
+++ FrontEnd.cpp
@@ -4,16 +4,7 @@ using namespace std;
 //                    A,B,C,D,E,F,G,F,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
 const int num[26] = { 7,7,7,7,7,7,7,7,3,7,7,7,9,8,7,7,7,7,7,7,7,7,7,7,7,7 };
 
-
-void PrStr(const char* name, int x, int y, int color) //prt str
-{
-	ChangeTextColor(color);
-	CPointer(x, y);
-	cout << name;
-	return;
-}
-
-void PaintFrame(int x, int y, int h, int w) // coordinates x,y ------ height --- width
+void DrawFrame(int x, int y, int h, int w) // coordinates x,y ------ height --- width
 {
 	for (int i = 0; i <= h + 1; i++)
 		for (int j = 0; j <= w + 1; j++)
@@ -62,40 +53,44 @@ void CPointer(int x, int y) //Change Pointer
 	return;
 }
 
-
-void ChangeText(string &s)
-{
-	for (int i = 0; i < s.size(); i++)
-		if (s[i] == '*') s[i] = char(254);
-		//else if (s[i] == 'W') s[i] = char(220);
-		//else if (s[i] == 'F') s[i] = char(223);
-		//else if (s[i] == '|') s[i] = char(248);
-		//else if (s[i] == '+') s[i] = char(176);
-		//else if (s[i] == 'X') s[i] = char(254);
-	return;
-}
-
-void DrawObject(const char* type, int x, int y)
-{
-	ifstream myfile(type);
-	int cnt = 0;
-	string s[20];
-	while (getline(myfile, s[cnt]))
-		++cnt;
-	for (int i = 0; i < cnt; i++)
-	{
-		ChangeText(s[i]);
-		CPointer(x, y + i);
-		cout << s[i];
-	}
-	myfile.close();
-	return;
-}
-
 void ChangeTextColor(int color)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 	return;
 }
 
+void ChangeText(string &s)
+{
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == 'B') ChangeTextColor(3);
+		else if (s[i] == 'b') ChangeTextColor(11);
+		else if (s[i] == 'W') ChangeTextColor(15);
+		else if (s[i] == 'R') ChangeTextColor(12);
+		else if (s[i] == 'r') ChangeTextColor(13);
+		else if (s[i] == 'Y') ChangeTextColor(14);
+		else if (s[i] == 'N') ChangeTextColor(4);
+		else if (s[i] == 'G') ChangeTextColor(2);
+		else if (s[i] == 'g') ChangeTextColor(10);
+		if (s[i] != ' ') s[i] = char(219);
+		cout << s[i];
+	}
+	return;
+}
 
+void DrawObject(const string link, int x, int y)
+{
+	ifstream file(link);
+	int tmp = 0;
+	string s[20];
+	while (getline(file, s[tmp]))
+		++tmp;
+	for (int i = 0; i < tmp; i++)
+	{
+		ChangeText(s[i]);
+		CPointer(x, y + i);
+		//cout << s[i];
+	}
+	file.close();
+	return;
+}
--- FrontEnd.h
+++ FrontEnd.h
@@ -8,14 +8,9 @@
 #include <conio.h>
 using namespace std;
 
-void PrStr(const char* name, int x, int y, int color);
-void OutputFunction(const char* name, int x, int y, bool chosen);
-void OutputString(string name, int x, int y, bool chosen);
 void ClearFrame(int x, int y, int h, int w);
 void CPointer(int x, int y);
 void ChangeText(string &s);
-void DrawObject(const char* type, int x, int y);
+void DrawObject(const string link, int x, int y);
+void DrawFrame(int x, int y, int h, int w);
 void ChangeTextColor(int color);
-void PaintFrame(int x, int y, int h, int w);
-bool InputChar(string &res, int x, int y, bool password, int bound, bool edit);
-int Select(int r, int c, int &Pos);
